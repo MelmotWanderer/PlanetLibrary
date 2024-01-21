00b5797023f084da34a3f73c8fafa6818782505b
Melmot <94366075+MelmotWanderer@users.noreply.github.com>
2024-01-21 11:31:42 +0300

    Add files via upload

---
+++ LR1/LR1/SearchPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    class SearchPresenter : IPresenter
+    {
+        private readonly ISearchView _view;
+        private readonly ISearchModel _module;
+
+        public SearchPresenter(ISearchView view, ISearchModel module)
+        {
+            _view = view;
+            _module = module;
+            _view.SearchedPlanet += SearchPlanet;
+        }
+
+        public void Run()
+        {
+            _view.Show();
+        }
+        private void SearchPlanet(string namePlanet)
+        {
+          
+            _view.UpdateListView(_module.FindPlanetInLibrary(namePlanet));
+        }
+    }
+}
---
+++ LR1/LR1/SearcherPlanet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR1
+{
+    public partial class SearcherPlanet : FormWithListView, ISearchView
+    {
+        public event Action OpenedSearchPlanet;
+        public event Action<string> SearchedPlanet;
+        public SearcherPlanet()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchPlanet_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void OpenSearchPlanet(object sender, EventArgs e)
+        {
+            OpenedSearchPlanet?.Invoke();
+        }
+        private void SearchPlanet(object sender, EventArgs e)
+        {
+            SearchedPlanet?.Invoke(textBox1.Text);
+        }
+        public void UpdateListView(Planet planet)
+        {
+            listView1.Items.Clear();
+            if(planet != null)
+            {
+                ListViewItem planetUI = CreateListViewItem(planet.Name, planet.EquatorialDiameter.ToString(), planet.Mass.ToString(), planet.OrbitalRadius.ToString(), planet.RotationPeriod.ToString(), planet.CountSatellites.ToString(), PlanetData.GetTypePlanet(planet.TypePlanet));
+
+                listView1.Items.Add(planetUI); 
+            }
+         
+        }
+    }
+}
